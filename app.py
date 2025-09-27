817ef3020d7d71ee5172318a1ee9ae0f251b60b2
wkdaud9 <wkdaud9@naver.com>
2025-09-27 16:05:39 +0900

    초기 서버 파일 생성

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+# auth_views를 추가로 import 합니다.
+from views import main_views, auth_views
+
+app = Flask(__name__)
+
+# 메인 페이지와 인증 Blueprint를 등록합니다.
+app.register_blueprint(main_views.bp)
+app.register_blueprint(auth_views.bp) # ◀ 이 줄이 추가되어야 합니다.
+
+if __name__ == '__main__':
+    app.run(debug=True)
