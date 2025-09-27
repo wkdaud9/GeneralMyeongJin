88e7b15fa7eac208520b530bf5c244a2b9d2a5e0
지수 <propose215@naver.com>
2025-09-27 18:59:21 +0900

    llm 모델 추가

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 # auth_views를 추가로 import 합니다.
-from views import main_views, auth_views, scraper_views, news_views
-
+from views import main_views, auth_views, scraper_views, news_views, llm_views
 app = Flask(__name__)
 
 # 메인 페이지와 인증 Blueprint를 등록합니다.
@@ -9,7 +8,8 @@ app.register_blueprint(main_views.bp)
 app.register_blueprint(auth_views.bp) # ◀ 이 줄이 추가되어야 합니다.
 app.register_blueprint(scraper_views.bp) # ◀ 이 줄을 추가합니다.
 app.register_blueprint(news_views.bp)
-#Hello
+app.register_blueprint(llm_views.bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
