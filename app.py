6e9a9a0bc5fa3b3cae8220a41cfdc71c4cb26029
wkdaud9 <wkdaud9@naver.com>
2025-09-27 16:46:33 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # 메인 페이지와 인증 Blueprint를 등록합니다.
 app.register_blueprint(main_views.bp)
 app.register_blueprint(auth_views.bp) # ◀ 이 줄이 추가되어야 합니다.
+app.register_blueprint(scraper_views.bp) # ◀ 이 줄을 추가합니다.
 
 if __name__ == '__main__':
     app.run(debug=True)
